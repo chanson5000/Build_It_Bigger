38136ceadc16a7ea33c7b547a1e7399fba21fbcf
Cory <chanson5000@gmail.com>
2018-07-30 16:06:10 -0500

    Now using an interface for callback.

--- app/src/main/java/com/udacity/gradle/builditbigger/JokeEndpointAsyncTask.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/JokeEndpointAsyncTask.java
@@ -1,49 +1,47 @@
 package com.udacity.gradle.builditbigger;
 
 import android.content.Context;
-
 import android.os.AsyncTask;
-import android.support.v4.util.Pair;
-import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+import com.udacity.gradle.builditbigger.util.GetJokeClickHandler;
 
 import java.io.IOException;
 
 
-class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
-    private static MyApi myApiService = null;
-    private Context context;
+class JokeEndpointAsyncTask extends AsyncTask<Context, Void, String> {
+
+    private static MyApi jokeApiService = null;
+    private GetJokeClickHandler mGetJokeClickHandler;
 
     @Override
-    protected String doInBackground(Pair<Context, String>... params) {
-        if(myApiService == null) {  // Only do this once
+    protected String doInBackground(Context... context) {
+        if (jokeApiService == null) {  // Only do this once
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
                     // - 10.0.2.2 is localhost's IP address in Android emulator
                     // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                    .setRootUrl("http://10.0.3.2:8080/_ah/api/")
                     .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
                         @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) {
                             abstractGoogleClientRequest.setDisableGZipContent(true);
                         }
                     });
             // end options for devappserver
 
-            myApiService = builder.build();
+            jokeApiService = builder.build();
         }
 
-        context = params[0].first;
-        String name = params[0].second;
+        mGetJokeClickHandler = (GetJokeClickHandler) context[0];
 
         try {
-            return myApiService.sayHi(name).execute().getData();
+            return jokeApiService.tellJoke().execute().getData();
         } catch (IOException e) {
             return e.getMessage();
         }
@@ -51,7 +49,7 @@ class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> 
 
     @Override
     protected void onPostExecute(String result) {
-        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        mGetJokeClickHandler.onResult(result);
     }
 }
 
