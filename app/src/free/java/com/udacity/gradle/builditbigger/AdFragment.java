08c80f1a767cced0d4cbaa44f0f951e1a504685b
Cory <chanson5000@gmail.com>
2018-07-30 18:24:52 -0500

    Added flavors

--- app/src/free/java/com/udacity/gradle/builditbigger/AdFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/AdFragment.java
@@ -21,7 +21,7 @@ public class AdFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.ad_fragment, container, false);
 
         AdView mAdView = (AdView) root.findViewById(R.id.adView);
         // Create an ad request. Check logcat output for the hashed device ID to
