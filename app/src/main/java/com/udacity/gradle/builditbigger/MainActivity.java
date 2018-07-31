31e846230054422fdc1215af91a6d5eb1cfd6d13
Cory <chanson5000@gmail.com>
2018-07-30 19:20:29 -0500

    Added a loading progress indicator.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 import com.nverno.jokeactivity.JokeActivity;
 import com.udacity.gradle.builditbigger.util.GetJokeClickHandler;
@@ -13,10 +15,16 @@ import com.udacity.gradle.builditbigger.util.GetJokeClickHandler;
 public class MainActivity extends AppCompatActivity implements GetJokeClickHandler {
     private static final String JOKE_STRING = "JOKE_STRING";
 
+    Button mButtonTellJoke;
+    ProgressBar mLoadingIndicator;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonTellJoke = findViewById(R.id.button_tell_joke);
+        mLoadingIndicator = findViewById(R.id.progress_bar);
     }
 
     @Override
@@ -42,13 +50,32 @@ public class MainActivity extends AppCompatActivity implements GetJokeClickHandl
     }
 
     public void tellJoke(View view) {
+        showLoadingIndicator();
         new JokeEndpointAsyncTask().execute(this);
     }
 
     public void onResult(String result) {
         final Intent intent = new Intent(this, JokeActivity.class);
         intent.putExtra(JOKE_STRING, result);
-
+        hideLoadingIndicator();
         startActivity(intent);
+    }
+
+    private void showLoadingIndicator() {
+        if (mButtonTellJoke.getVisibility() == View.VISIBLE) {
+            mButtonTellJoke.setVisibility(View.INVISIBLE);
+        }
+        if (mLoadingIndicator.getVisibility() == View.INVISIBLE) {
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void hideLoadingIndicator() {
+        if (mLoadingIndicator.getVisibility() == View.VISIBLE) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
+        }
+        if (mButtonTellJoke.getVisibility() == View.INVISIBLE) {
+            mButtonTellJoke.setVisibility(View.VISIBLE);
+        }
     }
 }
